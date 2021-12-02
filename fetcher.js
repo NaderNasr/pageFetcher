0a50533e522ccc62912fab49712b61dd961e415c
Nader Nasr <nadernasr7@gmail.com>
2021-12-01 21:57:01 -0500

    dynamic return

--- fetcher.js
+++ fetcher.js
@@ -1,25 +1,33 @@
 const fs = require('fs');
 const request = require('request');
-
+const readline = require('readline');
+const rl = readline.createInterface({input:process.stdin, output: process.stdout });
 
 let content = '';
+const fileName = 'file.log';
+const path = `/Users/nadernasr/Desktop/LHL/w2/d3 - net/pageFetcher/${fileName}`;
+
 
 request('http://www.nadernasr.ca', (error, response, body) => {
   content += error; // Print the error if one occurred
   content += response && response.statusCode; // Print the response status code if a response was received
   content += body; // Print the HTML for the Google homepage.
-  fs.writeFile('/Users/nadernasr/Desktop/LHL/w2/d3 - net/pageFetcher/file.log', content, err => {
+  fs.writeFile(path, content, err => {
+    
     if (err) {
       console.error(err);
       return;
     }
     //file written successfully
-    console.log('Please find - "file.log" in your directory');
+    rl.write(`Please find - ${fileName} in your directory \n`);
+    rl.close();
   });
 });
 
 
-
+// fs.access('/Users/nadernasr/Desktop/LHL/w2/d3 - net/pageFetcher/file.log'), fs.R_OK, (err) => {
+//   if (!err) { console.log("File exists");
+// };
 
 
 
